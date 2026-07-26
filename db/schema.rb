954356343233b57a0a04687a2fd1f6813980f19f
treva556 <108651221+treva556@users.noreply.github.com>
2026-07-26 12:25:59 +0300

    Chore/rails 8 upgrade (#11)
    * Upgrade Rails dependencies to 8.0.5
    
    * Update schema after Phase 1 migrations
    
    * Enable Rails 8 framework defaults

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_07_25_120605) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_25_120605) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "branches", force: :cascade do |t|
     t.bigint "organization_id", null: false
